1a48cc7ba024a29f96c4501b1ba6049fba4e2319
206620219 <Brian.DeMaio@universalorlando.com>
2020-01-10 16:20:14 -0500

    Adding in buttons to clear, update, remove, etc. for the part items

--- part_manager.py
+++ part_manager.py
@@ -31,10 +31,37 @@ price_label.grid(row=1, column=2, sticky=W)
 price_entry = Entry(app, textvariable=price_text)
 price_entry.grid(row=1, column=3)
 
-# Title and window width/height
-app.title('price Manager')
-app.geometry('700x350')
+# Parts List (Listbox)
+parts_list = Listbox(app, height=8, width=50, border=0)
+parts_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
 
+# Scrollbar for above Listbox
+scrollbar = Scrollbar(app)
+scrollbar.grid(row=3, column=3)
+
+# Set scroll to above Listbox
+parts_list.configure(yscrollcomman=scrollbar.set)
+scrollbar.configure(command=parts_list.yview)
+
+# Button for add part item
+add_btn = Button(app, text="Add Part", width=12, command=add_item)
+add_btn.grid(row=2, column=0, pady=20)
+
+# Button for remove part item
+remove_btn = Button(app, text="Remove Part", width=12, command=remove_item)
+remove_btn.grid(row=2, column=1)
+
+# Button for updating part item
+update_btn = Button(app, text="Update Part", width=12, command=update_item)
+update_btn.grid(row=2, column=2)
+
+# Buttons for clearing item input
+clear_btn = Button(app, text="Clear Input", width=12, command=clear_text)
+clear_btn.grid(row=2, column=0)
+
+# Title and window width/height
+app.title('Price Manager')
+app.geometry('700x350')
 
 # Start program
 app.mainloop()
