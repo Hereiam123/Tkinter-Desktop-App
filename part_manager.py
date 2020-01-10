6b7d146f2ceb68d5f1dbf57f6d3e182bbee4ab9e
206620219 <Brian.DeMaio@universalorlando.com>
2020-01-10 16:08:00 -0500

    Added in labels and text fields

--- part_manager.py
+++ part_manager.py
@@ -1,7 +1,40 @@
 from tkinter import *
 
-#Create window
+# Create window
 app = Tk()
 
-#Start program
+# price
+price_text = StringVar()
+price_label = Label(app, text='price Name', font=('bold', 14), pady=20)
+price_label.grid(row=0, column=0, sticky=W)
+price_entry = Entry(app, textvariable=price_text)
+price_entry.grid(row=0, column=1)
+
+# Customer
+customer_text = StringVar()
+customer_label = Label(app, text='Customer', font=('bold', 14))
+customer_label.grid(row=0, column=2, sticky=W)
+customer_entry = Entry(app, textvariable=customer_text)
+customer_entry.grid(row=0, column=3)
+
+# Retailer
+retailer_text = StringVar()
+retailer_label = Label(app, text='Retailer', font=('bold', 14), pady=20)
+retailer_label.grid(row=1, column=0, sticky=W)
+retailer_entry = Entry(app, textvariable=retailer_text)
+retailer_entry.grid(row=1, column=1)
+
+# Price
+price_text = StringVar()
+price_label = Label(app, text='Price', font=('bold', 14), pady=20)
+price_label.grid(row=1, column=2, sticky=W)
+price_entry = Entry(app, textvariable=price_text)
+price_entry.grid(row=1, column=3)
+
+# Title and window width/height
+app.title('price Manager')
+app.geometry('700x350')
+
+
+# Start program
 app.mainloop()
